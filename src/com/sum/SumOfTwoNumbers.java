4ffd8840e8d199e5ca42a390f1ae2a7a656d9880
anwe712 <ghoshanwesha272@gmail.com>
2026-03-12 10:32:51 +0530

    commit

--- src/com/sum/SumOfTwoNumbers.java
+++ src/com/sum/SumOfTwoNumbers.java
@@ -5,5 +5,8 @@ public class SumOfTwoNumbers {
 	public static void main(String[] args) {
 		int a = 5, b = 10;
 		System.out.println(a + b);
+		
+		int c = 10 , d = 100 ;
+		System.out.println(c+d);
 	}
 }
